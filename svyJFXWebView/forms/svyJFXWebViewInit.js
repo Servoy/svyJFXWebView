df8fb8b72f81c2aa37d46bcf297bd2b0919cb472
paronne <paronne@servoy.com>
2014-11-24 15:04:08 +0100

    include abstract  module definition

---
+++ svyJFXWebView/forms/svyJFXWebViewInit.js
@@ -0,0 +1,15 @@
+/**
+ * @override
+ * @properties={typeid:24,uuid:"2844A738-1C70-451E-B003-518D9B5CF053"}
+ */
+function getId() {
+	return 'com.servoy.bap.jfxwebview'
+}
+
+/**
+ * @override
+ * @properties={typeid:24,uuid:"0DA40A21-9E20-4E91-9D74-9EDD905F60F4"}
+ */
+function getVersion() {
+	return '1.0.0'
+}
